20bf324429e3f7d0bb0dcaca199092b81a90ed93
Tias <trisn.eclipse@gmail.com>
2024-07-13 17:37:45 +0600

    tour booking, thank you page

--- frontend/src/pages/TourDetails.jsx
+++ frontend/src/pages/TourDetails.jsx
@@ -7,6 +7,7 @@ import tourData from '../assets/data/tours'
 import calculateAvgRating from '../utils/avgRating'
 import avatar from '../assets/images/avatar.jpg'
 import Booking  from '../components/Booking/Booking'
+import NewsLetter from '../shared/NewsLetter'
 
 const TourDetails = () => {
   const {id} = useParams()
@@ -114,6 +115,7 @@ const TourDetails = () => {
           </Row>
         </Container>
       </section>
+      <NewsLetter />
     </>
   )
 }
--- frontend/src/router/Routers.js
+++ frontend/src/router/Routers.js
@@ -6,6 +6,7 @@ import Tours from './../pages/Tours'
 import TourDetails from './../pages/TourDetails'
 import Register from './../pages/Register'
 import SearchResultList from './../pages/SearchResultList'
+import { ThankYou } from '../pages/ThankYou'
 
 const Routers = () => {
   return (
@@ -16,6 +17,7 @@ const Routers = () => {
     <Route path='/tours/:id' element={<TourDetails />} />
     <Route path='/login' element={<Login />} />
     <Route path='/register' element={<Register />} />
+    <Route path='/thank-you' element={<ThankYou />} />
     <Route path='/search' element={<SearchResultList />} />
 
    </Routes>
